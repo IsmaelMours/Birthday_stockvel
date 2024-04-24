4374575fa3ed45b821cae4f71f5f7f5a4b04e1c5
Ismael Mours <83817080+IsmaelMours@users.noreply.github.com>
2024-04-24 14:52:38 +0200

    Add files via upload

---
+++ src/main/java/com/ismael/birthday_stockvel/BirthdayStockvelApplication.java
@@ -0,0 +1,13 @@
+package com.ismael.birthday_stockvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayStockvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayStockvelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ismael/birthday_stockvel/BirthdayStockvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismael.birthday_stockvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayStockvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
